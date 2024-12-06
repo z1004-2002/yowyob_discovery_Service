bf4419bce5540aea362f8bda1e331db63950eb2c
z1004-2002 <zogningfranc@gmail.com>
2024-12-06 20:00:36 +0100

    first commit

---
+++ src/test/java/discovey_service/discovery_Service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package discovey_service.discovery_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
